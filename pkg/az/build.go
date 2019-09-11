43c5880fd79cdf9e77c0017718fa2253dfce455c
Carolyn Van Slyck <carolyn.vanslyck@microsoft.com>
2019-09-11 12:08:30 -0500

    Install the iot extension

--- pkg/az/build.go
+++ pkg/az/build.go
@@ -8,5 +8,6 @@ func (m *Mixin) Build() error {
 	fmt.Fprintln(m.Out, `RUN curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /etc/apt/trusted.gpg.d/microsoft.asc.gpg`)
 	fmt.Fprintln(m.Out, `RUN echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $(lsb_release -cs) main" > /etc/apt/sources.list.d/azure-cli.list`)
 	fmt.Fprintln(m.Out, `RUN apt-get update && apt-get install -y azure-cli`)
+	fmt.Fprintln(m.Out, `RUN az extension add --name azure-cli-iot-ext`)
 	return nil
 }
